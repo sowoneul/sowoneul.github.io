41574e5896b58caa2bd4bef55e203c90124af01e
sowon <bchow27270@gmail.com>
2020-07-22 19:05:07 -0700

    Updates

---
+++ precache-manifest.1edc0faecd8418601e39402f3684f475.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43f9d449def7b082daef3f8e389b0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12213cc7430c7ce18d2",
+    "url": "/static/css/main.14a0d715.chunk.css"
+  },
+  {
+    "revision": "192561ea990c9c6127ed",
+    "url": "/static/js/2.698f60de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.698f60de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12213cc7430c7ce18d2",
+    "url": "/static/js/main.66dfc573.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "29cf710c60dd2dc63e4daabaaed68933",
+    "url": "/static/media/PoyoyoVibin.29cf710c.png"
+  },
+  {
+    "revision": "21463e77482a3e4a9a04523acb473446",
+    "url": "/static/media/exljbris_-_museo-300-webfont.21463e77.woff"
+  },
+  {
+    "revision": "4e4cfc943aba4a4f04a41e7a9a699d9f",
+    "url": "/static/media/exljbris_-_museo-300-webfont.4e4cfc94.woff2"
+  },
+  {
+    "revision": "0b4082802cb48a9d12b15e98158ce4f4",
+    "url": "/static/media/exljbris_-_museo-500-webfont.0b408280.woff"
+  },
+  {
+    "revision": "51b2afa21ad6761f53da0fe4f15c1d36",
+    "url": "/static/media/exljbris_-_museo-500-webfont.51b2afa2.woff2"
+  },
+  {
+    "revision": "37977cdc7de8ba644cd869c2ed4733f3",
+    "url": "/static/media/exljbris_-_museo-700-webfont.37977cdc.woff"
+  },
+  {
+    "revision": "9faccc7fdc5283d6aea4175ef5263b79",
+    "url": "/static/media/exljbris_-_museo-700-webfont.9faccc7f.woff2"
+  },
+  {
+    "revision": "93f872fd43a21a9e25a33bd4b7563b86",
+    "url": "/static/media/gacha.93f872fd.png"
+  },
+  {
+    "revision": "bd2ab779e49c4d12cc60a36a85d94ec4",
+    "url": "/static/media/gacha_preview.bd2ab779.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0262c295d811f8a8e118cf477decec30",
+    "url": "/static/media/menu.0262c295.svg"
+  },
+  {
+    "revision": "affef7e0a43b1c57f6d6783222b681a9",
+    "url": "/static/media/menu_open.affef7e0.svg"
+  },
+  {
+    "revision": "7101bec7ca2e5d4389ad57a6cdb34dbd",
+    "url": "/static/media/wip_preview.7101bec7.png"
+  }
+]);
